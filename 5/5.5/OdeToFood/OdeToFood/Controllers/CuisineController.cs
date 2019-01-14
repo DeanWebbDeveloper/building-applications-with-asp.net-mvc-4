bb369a8e7b3fd7cf3059e316e647596f129de84b
Dean Webb Developer <dean@deanwebbdeveloper.com>
2019-01-14 15:44:02 +0000

    5.4 - 6.3
    Chapter 5 Completed
    
    Started Chapter 6

---
+++ 5/5.5/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,18 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        public ActionResult Search(string name = "French")
+        {
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+        }
+    }
+}
---
+++ 6/6.3/OdeToFood/OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        // GET: Reviews
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+
+    }
+}
